78212bf51d506c4bafee19ea9bf29e8ea34de463
Toxkorengkengz07 <vito.korengkeng@gmail.com>
2024-08-09 17:27:42 +0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//  Hero type effect
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var typed = new Typed(".typed", {
+//     strings: [
+//       "I'm Vito Julio Korengkeng",
+//       "I'm Informatics student at Klabat University",
+//     ],
+//     typeSpeed: 50,
+//     backSpeed: 25,
+//     loop: true,
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".typed", {
+    strings: ["Vito Julio Korengkeng", "Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true,
+  });
+});
